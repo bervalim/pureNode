99ab4ae1795a808cdd7c22097721d8c5354b65db
bervalim <bernardogvalim@gmail.com>
2024-09-26 21:26:25 -0300

    studing routes

--- src/server.js
+++ src/server.js
@@ -1,7 +1,31 @@
 import http from "node:http"
+ //Rotas CRUD
+//  Requisição HTTP
+// Métodos mais comuns
+// Mais semânticos que de significado
+// GET ,POST,PUT,PATCH,DELETE
+
+// GET- BUSCAR recurso DO BACK
+// POST - Criar um recurso
+//PUT- Atualização total
+// PATCH- Atualização Parcial de um recurso no back
+// DELETE - Deletar recurso do back
 
 
 const server = http.createServer((req,res)=>{
+  const {method,url} = req
+  if(method==="GET" && url==='/users'){
+    // Early return
+    // Se o código bater nesse return, nada abaixo dele será executado
+    return res.end("Listagem usuários")
+  }
+  if(method ==="POST" && url==='/users' ){
+    return res.end("Criar usuário")
+  }
+
+  
+  console.log(method,url)
+
   res.end("Hello Worfdfldfdffdddçç")
 })
 
