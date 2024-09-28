ac1e5bcb1aa74f528637addee096df32bc443778
bervalim <bernardogvalim@gmail.com>
2024-09-28 20:46:58 -0300

    Status Code

--- src/server.js
+++ src/server.js
@@ -1,29 +1,25 @@
 import http from "node:http"
 
 const users = []
-// JSON - muito usado na transição entre front e back
-// e diferntes apis
-// Podemos usar vários tipos,e transitar vários dados entres várias aplicações
+
 const server = http.createServer((req,res)=>{
   const {method,url} = req
 
   if(method==="GET" && url==='/users'){
-  //  Esse cabeçalho irá dizer qual tipo de conteúdo estou retornando para o front
     return res.setHeader('Content-type','application/json').end(JSON.stringify(users))
   }
+
   if(method ==="POST" && url==='/users' ){
     users.push({
       id:1,
       name:'John Doe',
       email:"Joan@email.com"
     })
-    return res.end("Criar usuário")
+    return res.writeHead(201).end()
   }
 
-  
-  console.log(method,url)
 
-  res.end("Hello Word")
+  res.writeHead(404).end()
 })
 
 server.listen(3000)
