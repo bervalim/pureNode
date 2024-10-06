ba51a6af131b6d88fff8f74c8318d24fe80df76f
bervalim <bernardogvalim@gmail.com>
2024-10-06 09:56:18 -0300

    readble,writeble and transform streams

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -1,16 +1,7 @@
-// process- variável do node
-// stdin - diz respeito a tudo o que o usuário digita no terminal
-// Streams-> Ler dados de pouco a pouco e enviar para outra stream que irá tratar esses dados
-// process.stdin.pipe(process.stdout)
-// TUdo o que eu receber como entrada, será encaminhado pelo pipe para uma saída
-// stdout é uma stram de saída
-
-import {Readable} from 'node:stream'
+import {Readable,Writable,Transform} from 'node:stream'
 
 class OneToHundredStream extends Readable{
-  // Esse método irá retornar quais são os dados dessa stream
-  // Método para receber os dados de um uṕload
-  // Stream de leitura tem como função fornecer informações
+
   index = 1
   _read(){
     const i = this.index++
@@ -26,10 +17,24 @@ class OneToHundredStream extends Readable{
 
   }
 }
-// Estamos tentando ler a stream e enquanto estiver lendo a stream,
-// ele já irá escrever no terminal
-// Dentro de stream, não podemos trabalhar com strings ou números
-// O pedaço que estamos lendo ou escrevendo dentro de uma stream não pode estar
-// em um formato primitivo (sring,bboolrsn)
-// É necessário trabalhar em um outro formato do node (buffer)
-new OneToHundredStream().pipe(process.stdout)
+
+class InverseNumberStream extends Transform{
+  _transform(chunk,encoding,callback){
+    const transformed = Number(chunk.toString()) * -1
+    // primeiro parâmetro de um call back -e o erro
+    // enviamos null, caso não tenha acontecido nenhum erro dentro da transformação
+    callback(null,Buffer.from(String(transformed)))
+  }
+}
+class MultiplyByTenStream extends Writable{
+  
+  _write(chunk,enconding,callback){
+    console.log(Number(chunk.toString())*10)
+    callback()
+
+  }
+}
+// Lemos dados de uma stream de leitura  que retorna números de 1 a 100 e escrevemos esses dados dentro 
+// de uma stream de escrita
+
+new OneToHundredStream().pipe(new InverseNumberStream()).pipe(new MultiplyByTenStream())
