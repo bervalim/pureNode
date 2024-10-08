7b96d352e34b6e85c8262f1a6a1a315a3d71f8ea
bervalim <bernardogvalim@gmail.com>
2024-10-07 21:41:10 -0300

    JSON request body (STREAM AND buffers)

--- src/server.js
+++ src/server.js
@@ -2,18 +2,43 @@ import http from "node:http"
 
 const users = []
 
-const server = http.createServer((req,res)=>{
+const server = http.createServer(async (req,res)=>{
   const {method,url} = req
+
+ 
+// COnceito de leitura de stream
+  const buffers = []
+
+  for await(const chunk of req){
+   buffers.push(chunk)
+  }
+
+
+// Transformar um JSONjá criado em uma estrutura que possa
+// ser entendida pelo JS
+// Se não tiver req.body, iremos atribui-lo como null
+// Depois que lemos todo o corpo de requisição, o transformamos em um Objeto JavaScrit
+// Caso exista um objeto JS
+// Caso não exista, atribuímos req.body como null
+try{
+ req.body = JSON.parse(Buffer.concat(buffers).toString())
+}catch{
+  req.body = null
+}
+
+
 
   if(method==="GET" && url==='/users'){
     return res.setHeader('Content-type','application/json').end(JSON.stringify(users))
   }
 
   if(method ==="POST" && url==='/users' ){
+
+    const {name,email} = req.body
     users.push({
       id:1,
-      name:'John Doe',
-      email:"Joan@email.com"
+      name,
+      email
     })
     return res.writeHead(201).end()
   }
